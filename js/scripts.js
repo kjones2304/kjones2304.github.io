52b552868d50e30ffcd37c34727372a9a5a6f5f4
kjones2304 <j.kyle41@yahoo.com>
2016-08-06 19:30:18 -0700

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	$(".button-collapse").sideNav();
+	$('.parallax').parallax();
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+});
